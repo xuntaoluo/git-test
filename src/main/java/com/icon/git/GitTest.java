a6ffbe16cfc49792006681564746bf51cc8224f3
icon <icon@qq.com>
2022-05-04 19:25:06 +0800

    first commit

---
+++ src/main/java/com/icon/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.icon.git;
+
+/**
+ * ClassName: GitTest
+ * Description:
+ * date: 2022/5/4 19:14
+ *
+ * @author: icon
+ * @version:
+ * @since JDK 1.8
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
